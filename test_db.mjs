2727e74fa566dc03922742212f5a68d2e9f7b087
scraper-cos <kubilaycaliskn@gmail.com>
2026-01-12 14:38:00 +0300

    Create test_db.mjs

---
+++ test_db.mjs
@@ -0,0 +1,5 @@
+import { originals, dupes } from './src/data/db.js';
+
+console.log("DB Loaded Successfully");
+console.log("Originals count:", originals.length);
+console.log("Dupes count:", dupes.length);
